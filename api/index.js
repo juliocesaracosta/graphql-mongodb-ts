a20ab5c6c48c70f24c7585ba9bcb98870c61d677
julio cesar <jcesartrabajo@gmail.com>
2024-05-05 10:45:36 -0700

    se cambia el dist por api

--- api/index.js
+++ api/index.js
@@ -7,36 +7,27 @@ import { expressMiddleware } from "@apollo/server/express4";
 import resolvers from "./resolvers.js";
 import { readFileSync } from "fs";
 import { resolve, dirname } from "path";
-
 import { fileURLToPath } from 'url';
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-
 const PORT = process.env.PORT || 5050;
 const app = express();
-
 app.use(cors());
 app.use(express.json());
-
 //highlight-start
-const typeDefs = gql(
-  readFileSync(resolve(__dirname, "..", "schema.graphql"), {
+const typeDefs = gql(readFileSync(resolve(__dirname, "..", "schema.graphql"), {
     encoding: "utf-8",
-  })
-);
-
+}));
 const schema = buildSubgraphSchema({ typeDefs, resolvers });
 const server = new ApolloServer({
-  schema,
+    schema,
 });
 // Note you must call `start()` on the `ApolloServer`
 // instance before passing the instance to `expressMiddleware`
 await server.start();
-
 app.use("/api", cors(), json(), expressMiddleware(server));
-
 // start the Express server
 app.listen(PORT, () => {
-  console.log(`Server is running on port: ${PORT}`);
+    console.log(`Server is running on port: ${PORT}`);
 });
+//# sourceMappingURL=index.js.map
